5e10dcd8e7e942bf3f255b3fcc72abe12b9e6538
WintF <777den@gmail.com>
2022-09-07 21:14:37 +0300

    Open Regestry

--- WRMod.py
+++ WRMod.py
@@ -2,9 +2,23 @@
 
 import os
 import glob
+import winreg
+
 
 os.chdir('D:\Work_Folder')
 files = glob.glob('*.txt')
 
 print(files)
 
+
+def getPath():
+    # Open the key and return the handle object.
+    hKey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "COMODO")
+
+    # Read the value.
+    result = winreg.QueryValueEx(hKey, "AutoAdminLogon")
+
+    # Close the handle object.
+    result = result[0]
+    return result
+    winreg.CloseKey(hKey)
